7a1b11439c4e20c7527ff4fba10dd8f1256f04fa
Trong Nguyen Do <1851050159trong@ou.edu.vn>
2020-09-02 21:07:41 +0700

    Commit lần đầu
    abc

---
+++ Assets/Script/BirdController/BirdController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float bounceForce;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip flyClip, pingClip, DiedClip;
+
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        _BirdMoveMent();
+    }
+    void _BirdMoveMent()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            myBody.velocity = new Vector2(myBody.velocity.x, bounceForce);
+            audioSource.PlayOneShot(flyClip);
+        }
+
+        if (myBody.velocity.y > 0)
+        {
+            float angel = 0;
+            angel = Mathf.Lerp(0, 90, myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+
+        }
+        else if (myBody.velocity.y == 0)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+        {
+            float angel = 0;
+            angel = Mathf.Lerp(0, -90, -myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+
+        }
+    }
+}
