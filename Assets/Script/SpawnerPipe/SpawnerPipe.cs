8ba1eed79c663842235138adb636b12f58fe9b80
Trong Nguyen Do <1851050159trong@ou.edu.vn>
2020-09-03 23:50:45 +0700

    Spawner Pipe
    đàasdasdfasdfasd

---
+++ Assets/Script/SpawnerPipe/SpawnerPipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerPipe : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pipeHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( Spawner());
+    }
+
+   IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds (1);
+        Instantiate(pipeHolder, transform.position, Quaternion.identity);
+        StartCoroutine(Spawner());
+
+    }
+}
