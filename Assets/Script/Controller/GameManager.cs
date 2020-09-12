da7db2dc58b85ebfc3df9a8e04ab8935c3440e50
Trong Nguyen Do <1851050159trong@ou.edu.vn>
2020-09-12 15:52:12 +0700

    nút pause, resume, restart
    qwe

---
+++ Assets/Script/Controller/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+   void Awake ()
+    {
+
+    }
+    void _MakeSingleInstance ()
+    {
+        if (instance != null)
+            instance = this;
+    }
+}
--- Assets/Script/Controller/GamePlayController.cs
+++ Assets/Script/Controller/GamePlayController.cs
@@ -7,11 +7,15 @@ public class GamePlayController : MonoBehaviour
 {
     [SerializeField]
     private Button instructionButton; 
+
     [SerializeField]
     private Text scoreText, bestScoreText, endScoreText; //điểm số
+
     [SerializeField]
-    private GameObject gameOverPanel;
+    private GameObject gameOverPanel, pausePanel;
+
     public static GamePlayController instance; //đồng bộ các biến và các strcipt
+
     void _makeInstance ()
     {
         if (instance == null)       //khởi tạo
@@ -37,5 +41,27 @@ public class GamePlayController : MonoBehaviour
     public void _BirdDiedShowGamePanel()
     {
         gameOverPanel.SetActive(true);
+    }
+    public void _MenuButton()
+    {
+        Application.LoadLevel("MainMenu");
+        //Ngoài ra có thể dùng Application.LoadSence, nhưng phải báo thêm thư viện SenceManager
+    }
+    public void _StartGameButton()
+    {
+        // load vào sence được truyền vào theo chuỗi
+        Application.LoadLevel("GamePlay");
+        //để load lại màn hình ngay tại thời điểm người chơi over
+        //Application.LoadLevel(Application.loadedLevel); 
+    }
+    public void _PauseButton ()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+    public void _ResumeButton ()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);  
     }
 }
